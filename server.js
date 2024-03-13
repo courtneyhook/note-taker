d929395d3fdc8a7786ae890db6fab392b04927b8
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-03-13 17:43:23 -0500

    user can now delete previous notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const uuid = require("uuid");
+const uuid = require("./uuid");
 
 const app = express();
 const PORT = 3001;
@@ -32,14 +32,13 @@ app.post("/api/notes", (req, res) => {
   fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
     const notes = JSON.parse(data);
     const newNote = { ...req.body };
+    newNote.id = uuid();
     //adds the new note to the json file
     notes.push(newNote);
-    console.log(notes);
     const stringNotes = JSON.stringify(notes);
     //writes all the notes to the json file
     fs.writeFile(path.join(__dirname, "./db/db.json"), stringNotes, (err) => {
       if (err) throw err;
-      console.log("written");
       //sends a response back to the client
       res.json(stringNotes);
     });
@@ -48,6 +47,24 @@ app.post("/api/notes", (req, res) => {
 //});
 
 //works with deleteNote function
-//app.delete(`api/notes/:id, (req, res) => {});
+app.delete("/api/notes/:id", (req, res) => {
+  //gets the id from the note that was clicked
+  const newID = req.params.id;
+  fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+    const notes = JSON.parse(data);
+    for (let n = 0; n < notes.length; n++) {
+      if (notes[n].id === newID) {
+        //removes the entry with the matching index
+        notes.splice(n, 1);
+      }
+    }
+
+    let stringNotes = JSON.stringify(notes);
+    fs.writeFile(path.join(__dirname, "./db/db.json"), stringNotes, (err) => {
+      //sends a response back to the client
+      res.json(notes);
+    });
+  });
+});
 
 app.listen(PORT, () => console.log(`Listening...at http://localhost:${PORT}`));
